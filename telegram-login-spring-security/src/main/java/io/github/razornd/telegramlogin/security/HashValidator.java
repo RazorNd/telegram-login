edcd896b62889bac35598915cdb3563becea3336
Daniil Razorenov <razor@razornd.ru>
2025-12-23 21:54:12 +0700

    Add HashValidator for Telegram authentication validation
    - Implemented `HashValidator` to verify the authenticity of Telegram authentication data.
    - Added comprehensive test suite (`HashValidatorTest`) to validate correct and edge case behaviors.

---
+++ telegram-login-spring-security/src/main/java/io/github/razornd/telegramlogin/security/HashValidator.java
@@ -0,0 +1,103 @@
+/*
+ * Copyright 2025 Daniil Razorenov
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package io.github.razornd.telegramlogin.security;
+
+import org.jspecify.annotations.Nullable;
+
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
+import java.nio.charset.StandardCharsets;
+import java.security.InvalidKeyException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Comparator;
+import java.util.HexFormat;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class HashValidator implements TelegramAuthenticationValidator {
+
+    private static final String HMAC_ALGORITHM = "HmacSHA256";
+
+    private final byte[] secretKey;
+
+    public HashValidator(String botToken) {
+        secretKey = sha256(botToken);
+    }
+
+    @Override
+    public ValidationResult validate(TelegramAuthenticationToken token) {
+        var telegramUser = token.getPrincipal();
+
+        var dataCheckString = makeDataCheckString(telegramUser);
+
+        var hmac = hmac(dataCheckString);
+
+        final byte[] provided = HexFormat.of().parseHex(telegramUser.hash());
+
+        if (MessageDigest.isEqual(hmac, provided)) {
+            return ValidationResult.valid();
+        }
+
+        return ValidationResult.invalid("Invalid hash");
+    }
+
+    private String makeDataCheckString(TelegramUser telegramUser) {
+        return Stream.of(new DataPart("id", String.valueOf(telegramUser.id())),
+                         new DataPart("first_name", telegramUser.firstName()),
+                         new DataPart("last_name", telegramUser.lastName()),
+                         new DataPart("username", telegramUser.username()),
+                         new DataPart("photo_url", telegramUser.photoUrl()),
+                         new DataPart("auth_date", String.valueOf(telegramUser.authDate().getEpochSecond())))
+                     .filter(DataPart::hasValue)
+                     .sorted(Comparator.comparing(DataPart::name))
+                     .map(DataPart::toString)
+                     .collect(Collectors.joining("\n"));
+    }
+
+    private byte[] hmac(String dataCheckString) {
+        try {
+            var hmac = Mac.getInstance(HMAC_ALGORITHM);
+
+            hmac.init(new SecretKeySpec(secretKey, HMAC_ALGORITHM));
+
+            return hmac.doFinal(dataCheckString.getBytes(StandardCharsets.UTF_8));
+        } catch (NoSuchAlgorithmException | InvalidKeyException exception) {
+            throw new IllegalStateException(exception);
+        }
+    }
+
+    private byte[] sha256(String botToken) {
+        try {
+            return MessageDigest.getInstance("SHA-256").digest(botToken.getBytes(StandardCharsets.UTF_8));
+        } catch (NoSuchAlgorithmException exception) {
+            throw new IllegalStateException(exception);
+        }
+    }
+
+    private record DataPart(String name, @Nullable String value) {
+
+        boolean hasValue() {
+            return value != null;
+        }
+
+        @Override
+        public String toString() {
+            return name + "=" + value;
+        }
+    }
+}
---
+++ telegram-login-spring-security/src/test/java/io/github/razornd/telegramlogin/security/HashValidatorTest.java
@@ -0,0 +1,73 @@
+/*
+ * Copyright 2025 Daniil Razorenov
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package io.github.razornd.telegramlogin.security;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.Instant;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class HashValidatorTest {
+
+    HashValidator hashValidator = new HashValidator("2326476206:3g9iZTSFL5Pw5jaVrRw6em9Va2IEKgOuUXkVf");
+
+    @Test
+    void validate() {
+        var telegramUser = new TelegramUser(6787L,
+                                            Instant.ofEpochSecond(1766499044L),
+                                            "fc8fdb07f0cd97eed41f68fd7ee2e2b167d78be67bd55d657fa334b8960bf7b5",
+                                            "Lesia",
+                                            "Thane",
+                                            "cora5",
+                                            "https://t.me/i/userpic/320/hamptonkfaur7.uqh.jpg");
+
+        var actual = hashValidator.validate(new TelegramAuthenticationToken(telegramUser));
+
+        assertThat(actual).isEqualTo(ValidationResult.valid());
+    }
+
+    @Test
+    void validateShouldCorrectWorkWithNullFields() {
+        var telegramUser = new TelegramUser(567880L,
+                                            Instant.parse("2023-05-29T15:54:48Z"),
+                                            "31788de82422ffafed19d359888f2df0b301155cd030e293c701eeeb39d3d083",
+                                            null,
+                                            null,
+                                            "antwinew8yd",
+                                            null);
+
+        var actual = hashValidator.validate(new TelegramAuthenticationToken(telegramUser));
+
+        assertThat(actual).isEqualTo(ValidationResult.valid());
+    }
+
+    @Test
+    void validateShouldReturnIncorrectResultForWrongHash() {
+        var telegramUser = new TelegramUser(14984267L,
+                                            Instant.parse("2025-12-23T21:52:13Z"),
+                                            "31788de82422ffafed19d359888f2df0b301155cd030e293c701eeeb39d3d083",
+                                            null,
+                                            null,
+                                            "kyndraryu",
+                                            null);
+
+        var actual = hashValidator.validate(new TelegramAuthenticationToken(telegramUser));
+
+        assertThat(actual).isEqualTo(ValidationResult.invalid("Invalid hash"));
+    }
+}
